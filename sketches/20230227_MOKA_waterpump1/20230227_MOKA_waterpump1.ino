5ec055c82e0b0dc814cce76bb93f2149d416c2af
hohno-46466 <hohno.46466@gmail.com>
2023-02-28 18:41:18 -0400

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

--- sketches/20230227_MOKA_waterpump1/20230227_MOKA_waterpump1.ino
+++ sketches/20230227_MOKA_waterpump1/20230227_MOKA_waterpump1.ino
@@ -7,9 +7,17 @@
 const int LEDpins[3] = {11, 10, 9};
 int LEDs[3] = {0, 0, 0};
 
-// pina for pumpus and solenoides
+// pins for pumpus and solenoides
 const int Xpins[3] = {2, 3, 4};
 boolean Xprev[3] = { false, false, false };
+
+// pins for water level sensor
+const int WLSpins[3] = {A3, A4, A5};
+const int WLSpows[3] = {7, 6, 5};
+int WLSvals[3] = {0, 0, 0};
+const int WLS_LOW = 250;
+const int WLS_HIGH = 500;
+int WLSlimit[3] = {350, 350, 350};
 
 // pins for the Buttons
 const int SWpins[3] = {A3, A4, A5};
@@ -23,25 +31,39 @@ void setup() {
   // make the LEDLEDpins outputs:
   for (int i = 0; i <= 2; i++) {
     pinMode(LEDpins[i], OUTPUT);
+    digitalWrite(LEDpins[i], LOW);
     pinMode(Xpins[i], OUTPUT);
-    pinMode(SWpins[i], INPUT_PULLUP);
+    digitalWrite(Xpins[i], HIGH); // negative logic
+    pinMode(SWpins[i], INPUT);
+    pinMode(WLSpows[i], OUTPUT);
+    digitalWrite(WLSpows[i], LOW);
   }
 }
 
-void loop() {
+void loop() {}
+
+void loop2() {
   // print keyword and millis
   Serial.print("SW/Pot\t");
   Serial.print(millis()); Serial.print("\t");
 
   // print six numbers (three sets of Buttons and Pots) in one string with TABs:
   for (int i = 0; i <= 2; i++) {
-    boolean x = digitalRead(SWpins[i]);
-    if ( x != Xprev[i] ) {
-      // Any change in the status of this button is reflected in the LED and pump/solenoid.
-      analogWrite(LEDpins[i], x ? 255 : 0);
-      digitalWrite(Xpins[i], x ? HIGH : LOW);
-      Xprev[i] = x;
+    pinMode(SWpins[i], INPUT_PULLUP);
+    boolean x = (digitalRead(SWpins[i]) == LOW);
+    pinMode(SWpins[i], INPUT);
+    int s = readWLS(WLSpows[i], WLSpins[i]);
+    if (false /*s < WLSlimit[i]*/) {
+      // digitalWrite(Xpins[i], HIGH);
+    } else {
+      if ( x != Xprev[i] ) {
+        // Any change in the status of this button is reflected in the LED and pump/solenoid.
+        analogWrite(LEDpins[i], x ? 0 : 255);
+        digitalWrite(Xpins[i], x ? LOW : HIGH);
+        Xprev[i] = x;
+      }
     }
+    Serial.print(s); Serial.print("\t");
     Serial.print(x); Serial.print("\t");
     Serial.print(analogRead(POTpins[i])); Serial.print("\t");
   }
@@ -53,7 +75,7 @@ void loop() {
     while (Serial.available() > 0) {
       // Suppose every line contains three sets of numbers.
       for (int i = 0; i <= 2; i++) {
-        // If the number of digits in a line is not a multiple of 3, 
+        // If the number of digits in a line is not a multiple of 3,
         // then one or two single line breaks must be entered.
         // See also: https://www.arduino.cc/reference/en/language/functions/communication/serial/parseint/
         LEDs[i] = Serial.parseInt();
@@ -75,4 +97,13 @@ void loop() {
   } else {
     delay(200);
   }
+}
+
+//This is a function used to get the reading
+int readWLS(int _sensorPower, int _sensorPin) {
+  digitalWrite(_sensorPower, HIGH);  // Turn the sensor ON
+  delay(10);              // wait 10 milliseconds
+  int _val = analogRead(_sensorPin);    // Read the analog value form sensor
+  digitalWrite(_sensorPower, LOW);   // Turn the sensor OFF
+  return _val;             // send current reading
 }
