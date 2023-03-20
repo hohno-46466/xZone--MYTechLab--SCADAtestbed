62183085c6caf5de9da573087d8ce442df0d2abf
hohno-46466 <hohno.46466@gmail.com>
2023-03-20 21:19:18 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- scripts/DarshanaDemo01/Darshana-Ch0F3.sh
+++ scripts/DarshanaDemo01/Darshana-Ch0F3.sh
@@ -3,5 +3,5 @@
 BROKER1=broker.hivemq.com
 TOPIC1=Darshana/Ch0F3
 
-sh ./SCADAtestbed1-out.sh SW | awk '{printf "%s\n", $13; fflush();}' | mosquitto_pub -l -t $TOPIC1 -h $BROKER1
+sh ./SCADAtestbed1-out.sh "SW" | awk '{printf "%s\n", $13; fflush();}' | mosquitto_pub -l -t $TOPIC1 -h $BROKER1
 
--- scripts/DarshanaDemo01/Darshana-Ch2F3.sh
+++ scripts/DarshanaDemo01/Darshana-Ch2F3.sh
@@ -3,5 +3,5 @@
 BROKER1=broker.hivemq.com
 TOPIC1=Darshana/Ch2F3
 
-sh ./SCADAtestbed1-out.sh SW | awk '{printf "%s\n", $9; fflush();}' | mosquitto_pub -l -t $TOPIC1 -h $BROKER1
+sh ./SCADAtestbed1-out.sh "SW" | awk '{printf "%s\n", $9; fflush();}' | mosquitto_pub -l -t $TOPIC1 -h $BROKER1
 
